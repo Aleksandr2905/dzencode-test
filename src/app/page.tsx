b1dcf3b978285615bdcecc11f5749fe4bc05dc13
Aleksandr2905 <natalukha@ukr.net>
2024-10-05 14:43:05 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { io as ClientIO } from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
 
 export default function Home() {
   const [activeUsers, setActiveUsers] = useState(0);
@@ -10,17 +10,7 @@ export default function Home() {
   useEffect(() => {
     // const socketInitializer = async () => {
     //   await fetch('/api/socket');
-    //   const connection = io(
-    //     'https://dzencode-test-fe0258948483.herokuapp.com/'
-    //   );
-
-    const connection = (ClientIO as any)(
-      'https://dzencode-test-fe0258948483.herokuapp.com/',
-      {
-        path: '/api/socket',
-        addTrailingSlash: false,
-      }
-    );
+    const connection = io('https://dzencode-backend.onrender.com');
 
     connection.on('activeUsers', (count: number) => {
       setActiveUsers(count);
