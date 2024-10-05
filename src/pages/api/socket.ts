4e8a5a25021c988a83dca6eddcb8c251f8af427c
Aleksandr2905 <natalukha@ukr.net>
2024-10-05 14:46:03 +0300

    fix

--- src/pages/api/socket.ts
+++ src/pages/api/socket.ts
@@ -1,25 +1,25 @@
-import { NextApiRequest } from 'next';
-import { NextApiResponseServerIO } from '@/types/next';
+// import { NextApiRequest } from 'next';
+// import { NextApiResponseServerIO } from '@/types/next';
 
-import { Server } from 'socket.io';
+// import { Server } from 'socket.io';
 
-const SocketHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
-  if (res.socket.server.io) {
-    console.log('Socket is already running');
-  } else {
-    console.log('Socket is initializing');
-    const io = new Server(res.socket.server as any);
-    res.socket.server.io = io;
+// const SocketHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
+//   if (res.socket.server.io) {
+//     console.log('Socket is already running');
+//   } else {
+//     console.log('Socket is initializing');
+//     const io = new Server(res.socket.server as any);
+//     res.socket.server.io = io;
 
-    io.on('connection', (socket) => {
-      io.emit('activeUsers', io.engine.clientsCount);
+//     io.on('connection', (socket) => {
+//       io.emit('activeUsers', io.engine.clientsCount);
 
-      socket.on('disconnect', () => {
-        io.emit('activeUsers', io.engine.clientsCount);
-      });
-    });
-  }
-  res.end();
-};
+//       socket.on('disconnect', () => {
+//         io.emit('activeUsers', io.engine.clientsCount);
+//       });
+//     });
+//   }
+//   res.end();
+// };
 
-export default SocketHandler;
+// export default SocketHandler;
