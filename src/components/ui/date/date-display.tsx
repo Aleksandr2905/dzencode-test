3f4ce04efcb7071826a62a198a956006df6e1aad
Aleksandr2905 <natalukha@ukr.net>
2024-10-05 18:34:35 +0300

    fix date

--- src/components/ui/date/date-display.tsx
+++ src/components/ui/date/date-display.tsx
@@ -32,46 +32,85 @@
 //     </p>
 //   );
 // };
+// 'use client';
+
+// import { useState, useEffect } from 'react';
+// import { months } from '@/utils';
+
+// export const DateDisplay = () => {
+//   const [dayOfWeek, setDayOfWeek] = useState('');
+//   const [dateString, setDateString] = useState('');
+
+//   const formatDate = (
+//     date: Date
+//   ): { dayOfWeek: string; dateString: string } => {
+//     const dayOfWeek = date.toLocaleDateString('ru-RU', { weekday: 'long' });
+//     const dayOfMonth = date.getDate();
+//     const year = date.getFullYear();
+
+//     const formattedDayOfMonth = dayOfMonth < 10 ? `0${dayOfMonth}` : dayOfMonth;
+//     const month = months[date.getMonth()];
+
+//     const capitalizedDayOfWeek =
+//       dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1);
+
+//     return {
+//       dayOfWeek: capitalizedDayOfWeek,
+//       dateString: `${formattedDayOfMonth} ${month}, ${year}`,
+//     };
+//   };
+
+//   useEffect(() => {
+//     const currentDate = new Date();
+//     const { dayOfWeek, dateString } = formatDate(currentDate);
+//     setDayOfWeek(dayOfWeek);
+//     setDateString(dateString);
+//   }, []);
+
+//   return (
+//     <p className="font-inter text-base font-normal">
+//       {dayOfWeek}
+//       <br />
+//       {dateString}
+//     </p>
+//   );
+// };
+
 'use client';
 
 import { useState, useEffect } from 'react';
-import { months } from '@/utils';
 
 export const DateDisplay = () => {
-  const [dayOfWeek, setDayOfWeek] = useState('');
-  const [dateString, setDateString] = useState('');
-
-  const formatDate = (
-    date: Date
-  ): { dayOfWeek: string; dateString: string } => {
-    const dayOfWeek = date.toLocaleDateString('ru-RU', { weekday: 'long' });
-    const dayOfMonth = date.getDate();
-    const year = date.getFullYear();
-
-    const formattedDayOfMonth = dayOfMonth < 10 ? `0${dayOfMonth}` : dayOfMonth;
-    const month = months[date.getMonth()];
-
-    const capitalizedDayOfWeek =
-      dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1);
-
-    return {
-      dayOfWeek: capitalizedDayOfWeek,
-      dateString: `${formattedDayOfMonth} ${month}, ${year}`,
-    };
-  };
+  const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
-    const currentDate = new Date();
-    const { dayOfWeek, dateString } = formatDate(currentDate);
-    setDayOfWeek(dayOfWeek);
-    setDateString(dateString);
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000); // Обновляем каждую секунду
+
+    return () => clearInterval(intervalId); // Очищаем интервал при размонтировании компонента
   }, []);
 
+  const formatTime = (date: Date) => {
+    return date.toLocaleTimeString('ru-RU', {
+      hour: '2-digit',
+      minute: '2-digit',
+    });
+  };
+
+  const formatDate = (date: Date) => {
+    return date.toLocaleDateString('ru-RU', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+  };
+
   return (
-    <p className="font-inter text-base font-normal">
-      {dayOfWeek}
-      <br />
-      {dateString}
-    </p>
+    <div className="top-menu">
+      <p>{formatDate(currentTime)}</p>
+      <p>{formatTime(currentTime)}</p>
+    </div>
   );
 };
