7c18342f3128383e25b8cd4c47dcdb29d412305d
Aleksandr2905 <natalukha@ukr.net>
2024-10-05 19:03:56 +0300

    fix date

--- src/components/ui/date/date-display.tsx
+++ src/components/ui/date/date-display.tsx
@@ -1,45 +1,7 @@
-// import { months } from '@/utils';
-
-// export const DateDisplay = () => {
-//   const currentDate = new Date();
-
-//   const formatDate = (
-//     date: Date
-//   ): { dayOfWeek: string; dateString: string } => {
-//     const dayOfWeek = date.toLocaleDateString('ru-RU', { weekday: 'long' });
-//     const dayOfMonth = date.getDate();
-//     const year = date.getFullYear();
-
-//     const formattedDayOfMonth = dayOfMonth < 10 ? `0${dayOfMonth}` : dayOfMonth;
-//     const month = months[date.getMonth()];
-
-//     const capitalizedDayOfWeek =
-//       dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1);
-
-//     return {
-//       dayOfWeek: capitalizedDayOfWeek,
-//       dateString: `${formattedDayOfMonth} ${month}, ${year}`,
-//     };
-//   };
-
-//   const { dayOfWeek, dateString } = formatDate(currentDate);
-
-//   return (
-//     <p className="font-inter text-base font-normal">
-//       {dayOfWeek}
-//       <br />
-//       {dateString}
-//     </p>
-//   );
-// };
-'use client';
-
-import { useState, useEffect } from 'react';
 import { months } from '@/utils';
 
 export const DateDisplay = () => {
-  const [dayOfWeek, setDayOfWeek] = useState('');
-  const [dateString, setDateString] = useState('');
+  const currentDate = new Date();
 
   const formatDate = (
     date: Date
@@ -60,12 +22,7 @@ export const DateDisplay = () => {
     };
   };
 
-  useEffect(() => {
-    const currentDate = new Date();
-    const { dayOfWeek, dateString } = formatDate(currentDate);
-    setDayOfWeek(dayOfWeek);
-    setDateString(dateString);
-  }, []);
+  const { dayOfWeek, dateString } = formatDate(currentDate);
 
   return (
     <p className="font-inter text-base font-normal">
@@ -75,3 +32,46 @@ export const DateDisplay = () => {
     </p>
   );
 };
+// 'use client';
+
+// import { useState, useEffect } from 'react';
+// import { months } from '@/utils';
+
+// export const DateDisplay = () => {
+//   const [dayOfWeek, setDayOfWeek] = useState('');
+//   const [dateString, setDateString] = useState('');
+
+//   const formatDate = (
+//     date: Date
+//   ): { dayOfWeek: string; dateString: string } => {
+//     const dayOfWeek = date.toLocaleDateString('ru-RU', { weekday: 'long' });
+//     const dayOfMonth = date.getDate();
+//     const year = date.getFullYear();
+
+//     const formattedDayOfMonth = dayOfMonth < 10 ? `0${dayOfMonth}` : dayOfMonth;
+//     const month = months[date.getMonth()];
+
+//     const capitalizedDayOfWeek =
+//       dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1);
+
+//     return {
+//       dayOfWeek: capitalizedDayOfWeek,
+//       dateString: `${formattedDayOfMonth} ${month}, ${year}`,
+//     };
+//   };
+
+//   useEffect(() => {
+//     const currentDate = new Date();
+//     const { dayOfWeek, dateString } = formatDate(currentDate);
+//     setDayOfWeek(dayOfWeek);
+//     setDateString(dateString);
+//   }, []);
+
+//   return (
+//     <p className="font-inter text-base font-normal">
+//       {dayOfWeek}
+//       <br />
+//       {dateString}
+//     </p>
+//   );
+// };
