000720993358d944136f76195d974f11d8f649a6
Aleksandr2905 <natalukha@ukr.net>
2024-10-05 19:18:23 +0300

    fix time

--- src/components/ui/time/time-display.tsx
+++ src/components/ui/time/time-display.tsx
@@ -30,3 +30,39 @@
 //     </div>
 //   );
 // };
+
+'use client';
+
+import { useEffect, useState } from 'react';
+import ClockIcon from '@/../public/assets/icons/clock.svg';
+
+export const TimeDisplay = () => {
+  const [currentTime, setCurrentTime] = useState<Date>(new Date());
+  const hour = currentTime.toLocaleTimeString('ru-RU', {
+    hour: 'numeric',
+    minute: 'numeric',
+  });
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  // const formatTime = (date: Date) => {
+  //   return date.toLocaleTimeString('ru-RU', {
+  //     hour: '2-digit',
+  //     minute: '2-digit',
+  //   });
+  // };
+
+  return (
+    <div className="flex items-center gap-1">
+      <ClockIcon width={18} height={18} />
+      <p className="font-inter text-base font-normal" suppressHydrationWarning>
+        {hour}
+      </p>
+    </div>
+  );
+};
