1c1c050e7922bf3f91bee7c58b77e19b4831c1ac
Aleksandr2905 <natalukha@ukr.net>
2024-10-05 20:26:46 +0300

    fix time & added loader

---
+++ src/components/common/loader/loader.tsx
@@ -0,0 +1,5 @@
+import { BeatLoader } from 'react-spinners';
+
+export const Loader = () => {
+  return <BeatLoader color="#71DB41" size={18} />;
+};
--- src/components/ui/time/time-display.tsx
+++ src/components/ui/time/time-display.tsx
@@ -1,40 +1,8 @@
-// 'use client';
-
-// import { useEffect, useState } from 'react';
-// import ClockIcon from '@/../public/assets/icons/clock.svg';
-
-// export const TimeDisplay = () => {
-//   const [currentTime, setCurrentTime] = useState<Date>(new Date());
-
-//   useEffect(() => {
-//     const intervalId = setInterval(() => {
-//       setCurrentTime(new Date());
-//     }, 1000);
-
-//     return () => clearInterval(intervalId);
-//   }, [currentTime]);
-
-//   const formatTime = (date: Date) => {
-//     return date.toLocaleTimeString('ru-RU', {
-//       hour: '2-digit',
-//       minute: '2-digit',
-//     });
-//   };
-
-//   return (
-//     <div className="flex items-center gap-1">
-//       <ClockIcon width={18} height={18} />
-//       <p className="font-inter text-base font-normal">
-//         {formatTime(currentTime)}
-//       </p>
-//     </div>
-//   );
-// };
-
 'use client';
 
 import { useEffect, useState } from 'react';
 import ClockIcon from '@/../public/assets/icons/clock.svg';
+import { Loader } from '@/components/common/loader';
 
 export const TimeDisplay = () => {
   const [currentTime, setCurrentTime] = useState<Date | null>(null);
@@ -58,10 +26,19 @@ export const TimeDisplay = () => {
 
   return (
     <div className="flex items-center gap-1">
-      <ClockIcon width={18} height={18} />
-      <p className="font-inter text-base font-normal" suppressHydrationWarning>
-        {currentTime ? formatTime(currentTime) : '...'}
-      </p>
+      {currentTime ? (
+        <>
+          <ClockIcon width={18} height={18} />
+          <p
+            className="font-inter text-base font-normal"
+            suppressHydrationWarning
+          >
+            {currentTime ? formatTime(currentTime) : <Loader />}
+          </p>
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
